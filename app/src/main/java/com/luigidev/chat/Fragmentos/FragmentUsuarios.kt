451b57430956753b5712b29d01edc2ecf9c62cfe
Luis Gonzalez <luisgnzhdz@gmail.com>
2025-03-19 00:22:27 -0600

    [FEAT] add menu navigation

---
+++ app/src/main/java/com/luigidev/chat/Fragmentos/FragmentUsuarios.kt
@@ -0,0 +1,21 @@
+package com.luigidev.chat.Fragmentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.luigidev.chat.R
+
+class FragmentUsuarios : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_usuarios, container, false)
+    }
+
+
+}
--- app/src/main/java/com/luigidev/chat/MainActivity.kt
+++ app/src/main/java/com/luigidev/chat/MainActivity.kt
@@ -5,8 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luigidev.chat.Fragmentos.FragmentChats
+import com.luigidev.chat.Fragmentos.FragmentPerfil
+import com.luigidev.chat.Fragmentos.FragmentUsuarios
+import com.luigidev.chat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,59 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Access to the views
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        verFragmentoPerfil()
+
+        binding.bottomNV.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.item_perfil->{
+                    // Perfil visualization
+                    verFragmentoPerfil()
+                    true
+                }
+                R.id.item_usuarios->{
+                    verFragmentoUsuarios()
+                    true
+                }
+                R.id.item_chats->{
+                    verFragmentoChats()
+                    true
+                }
+                else->{
+                    false
+                }
+            }
+        }
+    }
+
+    private fun verFragmentoPerfil(){
+        binding.tvTitulo.text= "Perfil"
+
+        val fragment = FragmentPerfil()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentoFl.id, fragment, "Fragment Perfil")
+        fragmentTransaction.commit()
+    }
+
+    private fun verFragmentoUsuarios(){
+        binding.tvTitulo.text= "Usuarios"
+
+        val fragment = FragmentUsuarios()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentoFl.id, fragment, "Fragment Usuarios")
+        fragmentTransaction.commit()
+    }
+
+    private fun verFragmentoChats(){
+        binding.tvTitulo.text= "Chats"
+
+        val fragment = FragmentChats()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentoFl.id, fragment, "Fragment Chats")
+        fragmentTransaction.commit()
     }
 }
