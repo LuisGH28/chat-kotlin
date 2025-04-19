323ab1c15f34931e600e6192d9a0219c7fc1d337
Luis Gonzalez <luisgnzhdz@gmail.com>
2025-04-19 01:10:50 -0600

    [FIX]: Improve the UX of the 'Forgot Password' flow and implement its functionality.

--- app/src/main/java/com/luigidev/chat/OlvidePassword.kt
+++ app/src/main/java/com/luigidev/chat/OlvidePassword.kt
@@ -1,20 +1,78 @@
 package com.luigidev.chat
 
+import android.app.ProgressDialog
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.luigidev.chat.databinding.ActivityOlvidePasswordBinding
 
 class OlvidePassword : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOlvidePasswordBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_olvide_password)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityOlvidePasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere un momento")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.IbRegresar.setOnClickListener{
+            onBackPressedDispatcher.onBackPressed()
         }
+
+        binding.btnEnviar.setOnClickListener{
+            validarInformacion()
+        }
+    }
+
+    private var email = ""
+
+    private fun validarInformacion() {
+        email = binding.etEmail.text.toString().trim()
+
+        if(email.isEmpty()){
+            binding.etEmail.error = "Ingrese su email"
+            binding.etEmail.requestFocus()
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.etEmail.error = "Email no valido"
+            binding.etEmail.requestFocus()
+        }else{
+            enviarInstrucciones()
+        }
+    }
+
+    private fun enviarInstrucciones() {
+        progressDialog.setMessage("Enviando instrucciones a $email")
+        progressDialog.show()
+
+        firebaseAuth.sendPasswordResetEmail(email)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(
+                    this,
+                    "Instrucciones enviadas",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            .addOnFailureListener { e ->
+               progressDialog.dismiss()
+                Toast.makeText(
+                    this,
+                    "Fallo el envio de instrucciones debido a ${e.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
     }
 }
